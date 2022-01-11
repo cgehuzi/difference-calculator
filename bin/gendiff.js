6ed14db1cfec6192f81afc748333a88b573adc75
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-12 00:46:07 +0300

    feat(3): add genDiff to action on command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/gendiff.js';
+
 const program = new Command();
 
 program
@@ -9,6 +11,9 @@ program
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
