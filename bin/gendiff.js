ec891cfb49eb10bc7d7d838bed3a7562d70bfecc
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-11 12:27:52 +0300

    feat(bin): config commander on dendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
