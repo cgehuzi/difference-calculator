fad1c88b659f2cf25e854dae3646316b5e46a278
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-12 13:37:29 +0300

    fix(3): fix test file

--- __tests__/gendiff.step3.test.js
+++ __tests__/gendiff.step3.test.js
@@ -2,29 +2,26 @@ import { test, expect, afterAll } from '@jest/globals';
 import * as fs from 'fs';
 import gendiff from '../src/gendiff.js';
 
-let data1;
-let data2;
-
 afterAll(() => {
-  fs.unlinkSync('file1.json');
-  fs.unlinkSync('file2.json');
+  fs.unlinkSync('step3.file1.json');
+  fs.unlinkSync('step3.file2.json');
 });
 
-test('genDiff', () => {
-  data1 = {
+test('genDiff (step 3)', () => {
+  const data1 = {
     host: 'hexlet.io',
     timeout: 50,
     proxy: '123.234.53.22',
     follow: false,
   };
-  fs.writeFileSync('file1.json', JSON.stringify(data1, null, 2));
+  fs.writeFileSync('step3.file1.json', JSON.stringify(data1, null, 2));
 
-  data2 = {
+  const data2 = {
     timeout: 20,
     verbose: true,
     host: 'hexlet.io',
   };
-  fs.writeFileSync('file2.json', JSON.stringify(data2, null, 2));
+  fs.writeFileSync('step3.file2.json', JSON.stringify(data2, null, 2));
 
   const diffs = `{
   - follow: false
@@ -35,5 +32,5 @@ test('genDiff', () => {
   + verbose: true
 }`;
 
-  expect(gendiff('file1.json', 'file2.json')).toEqual(diffs);
+  expect(gendiff('step3.file1.json', 'step3.file2.json')).toEqual(diffs);
 });
