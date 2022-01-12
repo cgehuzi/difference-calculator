beef3219220c4ac7645ce89852d4e43647141e9f
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-12 18:34:05 +0300

    fix(test): add genDiff(.json, .yml) test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import gendiff from '../src/gendiff.js';
+import genDiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,18 +10,26 @@ const __dirname = dirname(__filename);
 const getFixteruPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixteruPath(filename), 'utf-8');
 
-test('gendiff(.json, .json)', () => {
+test('genDiff(.json, .json)', () => {
   const file1 = getFixteruPath('file1.json');
   const file2 = getFixteruPath('file2.json');
   const diffs = readFile('diffs');
 
-  expect(gendiff(file1, file2)).toEqual(diffs);
+  expect(genDiff(file1, file2)).toEqual(diffs);
 });
 
-test('gendiff(.yml, .yml)', () => {
+test('genDiff(.yml, .yml)', () => {
   const file1 = getFixteruPath('file1.yml');
   const file2 = getFixteruPath('file2.yaml');
   const diffs = readFile('diffs');
 
-  expect(gendiff(file1, file2)).toEqual(diffs);
+  expect(genDiff(file1, file2)).toEqual(diffs);
+});
+
+test('genDiff(.json, .yml)', () => {
+  const file1 = getFixteruPath('file1.json');
+  const file2 = getFixteruPath('file2.yaml');
+  const diffs = readFile('diffs');
+
+  expect(genDiff(file1, file2)).toEqual(diffs);
 });
