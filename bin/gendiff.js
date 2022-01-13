ee376919c51e35e132aa7f157749966b8247b2c6
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-13 21:57:43 +0300

    fix(bin): add default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import genDiff from '../src/gendiff.js';
+import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -9,11 +10,15 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    switch (program.opts().format) {
+      case 'stylish':
+        console.log(genDiff(filepath1, filepath2));
+        break;
+    }
   });
 
 program.parse();
