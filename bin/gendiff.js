5a1806d774cd40d3f627f22bdead94b046348ccf
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-14 01:37:41 +0300

    fix(bin): add formatters choosing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,6 @@
 
 import { Command } from 'commander';
 import genDiff from '../src/gendiff.js';
-import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -10,19 +9,12 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    switch (program.opts().format) {
-      case 'stylish':
-        console.log(genDiff(filepath1, filepath2, stylish));
-        break;
-
-      default:
-        console.log(genDiff(filepath1, filepath2));
-        break;
-    }
+    const formatName = program.opts().format;
+    console.log(genDiff(filepath1, filepath2, formatName));
   });
 
 program.parse();
