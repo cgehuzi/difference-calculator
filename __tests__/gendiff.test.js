df3578a317748491eaa8c095613b2d4bc66623e9
Ihar Spurhiash <mail.cgehuzi@gmail.com>
2022-01-14 14:18:39 +0300

    feat(test): add tests for json formatter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -57,3 +57,27 @@ test('plain - tree structure', () => {
   expect(genDiff(yaml1, yaml2, 'plain')).toEqual(diffs);
   expect(genDiff(yaml1, json2, 'plain')).toEqual(diffs);
 });
+
+test('json - line structure', () => {
+  const json1 = getFixteruPath('line_file1.json');
+  const yaml1 = getFixteruPath('line_file1.yml');
+  const json2 = getFixteruPath('line_file2.json');
+  const yaml2 = getFixteruPath('line_file2.yaml');
+  const diffs = readFile('line_json_diffs');
+
+  expect(genDiff(json1, json2, 'json')).toEqual(diffs);
+  expect(genDiff(yaml1, yaml2, 'json')).toEqual(diffs);
+  expect(genDiff(yaml1, json2, 'json')).toEqual(diffs);
+});
+
+test('json - tree structure', () => {
+  const json1 = getFixteruPath('tree_file1.json');
+  const yaml1 = getFixteruPath('tree_file1.yml');
+  const json2 = getFixteruPath('tree_file2.json');
+  const yaml2 = getFixteruPath('tree_file2.yaml');
+  const diffs = readFile('tree_json_diffs');
+
+  expect(genDiff(json1, json2, 'json')).toEqual(diffs);
+  expect(genDiff(yaml1, yaml2, 'json')).toEqual(diffs);
+  expect(genDiff(yaml1, json2, 'json')).toEqual(diffs);
+});
